ec429bcbb08fd1ea744b29aac24bde67b25da788
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-02-17 00:17:01 +0530

    initial

--- wrh_bot/urls.py
+++ wrh_bot/urls.py
@@ -20,58 +20,9 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
 
-import asyncio
-from time import sleep
-import httpx
-from django.http import HttpResponse
-import random
-
-import discord
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = 'OTQzMTExNTM2Nzk1MzQ5MDAy.YguTEA.4qDsOQflIZ1MQQz62SfDfRbFpWM'
-GUILD = 'general'
-client = discord.Client()
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    if message.content == '99':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
-
-
-async def http_call_async():
-  for num in range(1,6):
-    await asyncio.sleep(1)
-    print(num)
-  async with httpx.AsyncClient() as client:
-    r = await client.get("https://httpbin.org")
-    print(r)
-
-async def async_view(request):
-  loop = asyncio.get_event_loop()
-  loop.create_task(http_call_async())
-  return HttpResponse('Non-blocking HTTP request')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", async_view)
 ]
 
 
